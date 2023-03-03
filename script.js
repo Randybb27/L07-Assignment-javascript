a44759ccd0414073d21ac4ad28ee2c03d56f1c48
Randy Beebe <randybb27@gmail.com>
2023-03-02 22:02:29 -0600

    "javascript practice"

---
+++ script.js
@@ -0,0 +1,16 @@
+var grocery =["Bananas", "Milk", "Eggs", "Bacon"]
+console.log(grocery)
+
+var message =["Please pick up the following from the store:Bananas", "Milk", "Eggs", "Bacon"]
+
+var i = 1
+
+for (var i=0; i < grocery.length; i++) {
+    console.log(grocery[i]);
+}
+
+if (grocery == 4) {
+    message = true;
+}
+
+console.log(message);
